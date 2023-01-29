edffd2701e2f8542cd3ae3b3a7782ddc77fb7f2e
Dariusz Rybicki <darrarski@gmail.com>
2023-01-29 22:05:40 +0100

    Remove alignment parameter from SimpleTableView

--- Sources/SimpleTable/SimpleTableView.swift
+++ Sources/SimpleTable/SimpleTableView.swift
@@ -16,26 +16,22 @@ public struct SimpleTableView<Content>: View where Content: View {
   ///   - scrollMode: Scroll view mode. Defaults to `.automatic`.
   ///   - scrollAxes: Scrolling axes. Defaults to `.vertical` & `.horizontal`.
   ///   - showsScrollIndicators: Determines if scroll indicators should be visible. Defaults to `true`.
-  ///   - alignment: Table content alignment. Defaults to `.topLeading`.
   ///   - content: Table content. Use `SimpleTableLayout` to arrange views in table.
   public init(
     scrollMode: ScrollMode = .automatic,
     scrollAxes: Axis.Set = [.vertical, .horizontal],
     showsScrollIndicators: Bool = true,
-    alignment: Alignment = .topLeading,
     @ViewBuilder content: @escaping () -> Content
   ) {
     self.scrollMode = scrollMode
     self.scrollAxes = scrollAxes
     self.showsScrollIndicators = showsScrollIndicators
-    self.alignment = alignment
     self.content = content
   }
 
   public var scrollAxes: Axis.Set
   public var scrollMode: ScrollMode
   public var showsScrollIndicators: Bool
-  public var alignment: Alignment
   public var content: () -> Content
   @State var scrollViewSize: CGSize = .zero
   let startAnchorId = "simple-table-start-anchor-id"
@@ -63,7 +59,7 @@ public struct SimpleTableView<Content>: View where Content: View {
     @ViewBuilder content: @escaping () -> Content
   ) -> some View {
     ScrollViewReader { scrollViewProxy in
-      ZStack(alignment: alignment) {
+      ZStack {
         GeometryReader { geometryProxy in
           Color.clear
             .onAppear { scrollViewSize = geometryProxy.size }
@@ -79,9 +75,9 @@ public struct SimpleTableView<Content>: View where Content: View {
             .frame(
               minWidth: scrollViewSize.width,
               minHeight: scrollViewSize.height,
-              alignment: alignment
+              alignment: .topLeading
             )
-            .overlay(alignment: alignment) {
+            .overlay(alignment: .topLeading) {
               Color.clear.frame(width: 10, height: 10)
                 .allowsHitTesting(false)
                 .id(startAnchorId)
